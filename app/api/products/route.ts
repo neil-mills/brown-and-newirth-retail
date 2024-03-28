0f575f3c701fcc5c73443b1575113d0b78f608ba
ross Lammin <ross@adigi.co.uk>
2024-03-28 12:14:47 +0000

    Changed product api route to the new one

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -5,7 +5,8 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function GET(request: NextRequest) {
   const res = await axios.get<Product[]>(
     // 'https://www.brownandnewirth.com/wp-json/productData/v1/data'
-    'https://www.brownandnewirth.com/product-data/get-products.json'
+    // 'https://www.brownandnewirth.com/product-data/get-products.json'
+    'https://staging.retailer.brownandnewirth.com/cache-data/product-data/get-products.json'
   )
   return NextResponse.json(res.data)
 }
