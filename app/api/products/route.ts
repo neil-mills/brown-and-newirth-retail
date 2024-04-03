cb0b0010d4683576a13eeebf695645af960a160c
Neil Mills <mail@neil-mills.co.uk>
2024-04-03 12:33:19 +0100

    Update products endpoint url

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const res = await axios.get<Product[]>(
-    'https://www.brownandnewirth.com/product-data/get-products.json' /// Test data
-    //'https://staging.retailer.brownandnewirth.com/cache-data/product-data/get-products.json' // Live Data
+    // 'https://www.brownandnewirth.com/product-data/get-products.json' /// Test data
+    'https://staging.retailer.brownandnewirth.com/cache-data/product-data/get-products.json' // Live Data
   )
   return NextResponse.json(res.data)
 }
