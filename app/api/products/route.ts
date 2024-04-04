5c39bf325b8b39e93cd7f4c7ce4207b4ac542295
Neil Mills <mail@neil-mills.co.uk>
2024-04-04 11:58:43 +0100

    Load products in chunks

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,10 +3,9 @@ import axios from 'axios'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
+  const chunk = request.nextUrl.searchParams.get('chunk')
   const res = await axios.get<Product[]>(
-    'https://www.brownandnewirth.com/product-data/get-products.json', /// Test data
-    // 'https://staging.retailer.brownandnewirth.com/cache-data/product-data/get-products.json', // Live Data
-    { timeout: 120000 }
+    `https://staging.retailer.brownandnewirth.com/cache-data/product-data/product-data-${chunk}.json`
   )
   return NextResponse.json(res.data)
 }
--- data/fetchData.ts
+++ data/fetchData.ts
@@ -1,17 +1,26 @@
 import axios, { AxiosError } from 'axios'
 import { Product } from '@/app/types'
 
-const fetchData = async () => {
+const fetchData = async (): Promise<Product[]> => {
+  const url = 'http://127.0.0.1:3000/api/products'
+  const totalChunks = 7
+  let products: Product[] = []
+  const endpoints = Array.from({ length: totalChunks }).map(
+    (_item, i) => `${url}?chunk=${i + 1}`
+  )
   try {
-    const res = await axios.get<Product[]>(
-      'http://127.0.0.1:3000/api/products',
-      { timeout: 120000 }
+    const responses = await axios.all(
+      endpoints.map((endpoint) => axios.get(endpoint))
     )
-    return res.data
+    products = responses.reduce((acc, res) => {
+      return [...acc, ...res.data]
+    }, [] as Product[])
   } catch (err) {
     const error = err as AxiosError
-    console.log(`RESPONSE FROM API IS: ${error.message}`)
+    console.log(`Error: ${error.message}`)
   }
+  console.log(products)
+  return products
 }
 
 export default fetchData
