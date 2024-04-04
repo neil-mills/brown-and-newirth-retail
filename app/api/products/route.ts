9187c35a73c10c58e631d52ed7f9e4b81d7ad149
Neil Mills <mail@neil-mills.co.uk>
2024-04-04 11:48:53 +0100

    Add try catch to axios requests

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,8 +4,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const res = await axios.get<Product[]>(
-    // 'https://www.brownandnewirth.com/product-data/get-products.json' /// Test data
-    'https://staging.retailer.brownandnewirth.com/cache-data/product-data/get-products.json' // Live Data
+    'https://www.brownandnewirth.com/product-data/get-products.json', /// Test data
+    // 'https://staging.retailer.brownandnewirth.com/cache-data/product-data/get-products.json', // Live Data
+    { timeout: 120000 }
   )
   return NextResponse.json(res.data)
 }
--- data/fetchData.ts
+++ data/fetchData.ts
@@ -1,9 +1,17 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { Product } from '@/app/types'
 
 const fetchData = async () => {
-  const res = await axios.get<Product[]>('/api/products')
-  return res.data
+  try {
+    const res = await axios.get<Product[]>(
+      'http://127.0.0.1:3000/api/products',
+      { timeout: 120000 }
+    )
+    return res.data
+  } catch (err) {
+    const error = err as AxiosError
+    console.log(`RESPONSE FROM API IS: ${error.message}`)
+  }
 }
 
 export default fetchData
