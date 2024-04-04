7ae9760f01bb5576388cca6460a81ee632143942
Neil Mills <mail@neil-mills.co.uk>
2024-04-04 14:07:44 +0100

    [debug] attempt to return all 7 chunks from api

--- data/fetchData.ts
+++ data/fetchData.ts
@@ -4,17 +4,28 @@ axios.defaults.timeout === 120000
 
 const fetchData = async () => {
   const url = '/api/products'
-  const req1 = await axios.get<Product[]>(`${url}?chunk=4`)
-  const req2 = await axios.get<Product[]>(`${url}?chunk=5`)
-  const req3 = await axios.get<Product[]>(`${url}?chunk=6`)
-  const req4 = await axios.get<Product[]>(`${url}?chunk=7`)
+  const req1 = await axios.get<Product[]>(`${url}?chunk=1`)
+  const req2 = await axios.get<Product[]>(`${url}?chunk=2`)
+  const req3 = await axios.get<Product[]>(`${url}?chunk=3`)
+  const req4 = await axios.get<Product[]>(`${url}?chunk=4`)
+  const req5 = await axios.get<Product[]>(`${url}?chunk=5`)
+  const req6 = await axios.get<Product[]>(`${url}?chunk=6`)
+  const req7 = await axios.get<Product[]>(`${url}?chunk=7`)
   let products: Product[] = []
   const totalChunks = 7
-  const requests = Array.from({ length: totalChunks }).map(
-    async (_item, i) => await axios.get<Product[]>(url)
-  )
+  // const requests = Array.from({ length: totalChunks }).map(
+  //   async (_item, i) => await axios.get<Product[]>(url)
+  // )
   try {
-    const responses = await axios.all([req1, req2, req3, req4])
+    const responses = await Promise.all([
+      req1,
+      req2,
+      req3,
+      req4,
+      req5,
+      req6,
+      req7,
+    ])
     console.log(responses)
     products = responses.reduce((acc, res) => {
       return [...acc, ...res.data]
