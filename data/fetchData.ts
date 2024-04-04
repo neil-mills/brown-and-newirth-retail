87fa2f68ef92ba3c02007e100f8ab920ec5d53a6
Neil Mills <mail@neil-mills.co.uk>
2024-04-04 12:42:31 +0100

    [debug] remove NEXT_PUBLIC_VERCEL_URL env from api url

--- data/fetchData.ts
+++ data/fetchData.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios'
 import { Product } from '@/app/types'
 
 const fetchData = async (): Promise<Product[]> => {
-  const url = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products`
+  const url = `/api/products`
   const totalChunks = 7
   let products: Product[] = []
   const endpoints = Array.from({ length: totalChunks }).map(
