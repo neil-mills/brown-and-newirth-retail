41cc97fbcabb60bc28d6fd9b8e2f3aa3733b1557
Neil Mills <mail@neil-mills.co.uk>
2024-04-04 14:52:41 +0100

    Reapply "Fix ceramic colour filtering menu"
    This reverts commit 365519916ec877efbf0c11fcd78c0f34843ea16b.

--- app/maps/ceramicColours.ts
+++ app/maps/ceramicColours.ts
@@ -1,20 +1,20 @@
 export const ceramicColoursMap = {
-  BLACK: {
+  Black: {
     label: 'Black',
     slug: 'black',
     image: '/img/svg/icon-ceramic-black.svg',
   },
-  GREY: {
+  Grey: {
     label: 'Grey',
     slug: 'grey',
     image: '/img/svg/icon-ceramic-grey.svg',
   },
-  BLUE: {
+  Blue: {
     label: 'Blue',
     slug: 'grey',
     image: '/img/svg/icon-ceramic-grey.svg',
   },
-  RED: {
+  Red: {
     label: 'Red',
     slug: 'red',
     image: '/img/svg/icon-ceramic-red.svg',
