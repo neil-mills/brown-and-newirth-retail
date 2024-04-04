8f58df405306c86bca2d2eeef0081878aae30bbb
Neil Mills <mail@neil-mills.co.uk>
2024-04-04 13:17:02 +0100

    [debug] use basic implementation of Promise.all

--- data/fetchData.ts
+++ data/fetchData.ts
@@ -2,9 +2,25 @@ import axios, { AxiosError } from 'axios'
 import { Product } from '@/app/types'
 
 const fetchData = async () => {
-  const res = await axios.get<Product[]>('/api/products')
+  const url = '/api/products'
+  const req = await axios.get<Product[]>(url)
+  const req2 = await axios.get<Product[]>(url)
+  let products: Product[] = []
+  const totalChunks = 7
+  // const endpoints = Array.from({ length: totalChunks }).map(
+  //   (_item, i) => `${url}?chunk=${i + 1}`
+  // )
+  try {
+    const responses = await Promise.all([req, req2])
+    products = responses.reduce((acc, res) => {
+      return [...acc, ...res.data]
+    }, [] as Product[])
+  } catch (err) {
+    const error = err as AxiosError
+    console.log(`Error: ${error.message}`)
+  }
   // await wait(4000)
-  return res.data
+  return products
 }
 /*
 const fetchData = async (): Promise<Product[]> => {
