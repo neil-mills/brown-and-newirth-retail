708a0fe0e3f8040f5fba9d2a91ec28c75e05595e
Neil Mills <mail@neil-mills.co.uk>
2024-04-06 08:49:18 +0100

    Sort profile filter options by mapping index

--- app/maps/profiles.ts
+++ app/maps/profiles.ts
@@ -7,11 +7,13 @@ export const profilesMap: PatternMap = {
     label: 'Barrel',
     image: '/img/svg/icon-profile-barrel.svg',
     slug: 'barrel',
+    index: 3,
   },
   'CLASSIC COURT': {
     label: 'Classic Court',
     image: '/img/svg/icon-profile-classic-court.svg',
     slug: 'classic-court',
+    index: 1,
   },
   Flat: {
     label: 'Flat',
@@ -22,15 +24,18 @@ export const profilesMap: PatternMap = {
     label: 'Modern Court',
     image: '/img/svg/icon-profile-modern-court.svg',
     slug: 'modern-court',
+    index: 2,
   },
   OTHER: {
     label: 'Other',
     image: '/img/svg/icon-profile-other.svg',
     slug: 'other',
+    index: 4,
   },
   'D SHAPED': {
     label: 'D-Shaped',
     image: '/img/svg/icon-profile-d-shaped.svg',
     slug: 'd-shaped',
+    index: 0,
   },
 }
