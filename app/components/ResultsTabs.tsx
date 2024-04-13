36804fd20fdf8c7f7f5aae0effd6fb56ba4e3a01
Neil Mills <mail@neil-mills.co.uk>
2024-04-13 10:12:29 +0100

    Fix tabs state

--- app/components/ResultsTabs.tsx
+++ app/components/ResultsTabs.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
 import { useStore } from '../hooks'
 
 export const ResultsTabs = () => {
   const { otherOptions, similarProducts } = useStore(
     (store) => store.selectedSku
   )
+  const showOtherOptions =
+    otherOptions.length > 0 || similarProducts.length === 0
+  const showSimilarProducts =
+    otherOptions.length === 0 && similarProducts.length > 0
   return (
     <div className="result-tabs col-pad-sm">
       <div className="nav row g-0" role="tablist">
         <div className="col-6 col-lg-12 col-xl-6">
           <button
-            className="nav-link w-100 active"
+            className={`nav-link w-100${showOtherOptions ? ' active' : ''}`}
             data-bs-toggle="tab"
             data-bs-target="#other"
             type="button"
@@ -21,7 +24,7 @@ export const ResultsTabs = () => {
         </div>
         <div className="col-6 col-lg-12 col-xl-6">
           <button
-            className="nav-link w-100"
+            className={`nav-link w-100${showSimilarProducts ? ' active' : ''}`}
             data-bs-toggle="tab"
             data-bs-target="#similar"
             type="button"
